bb9790aba98c8d6e12ae95ecaa45c5901085367b
Oscar Raig <oscar.raig@gmail.com>
2015-10-17 18:23:52 +0200

    First test

--- state-pattern/src/test/java/raig/org/GumballStateMachineTest.java
+++ state-pattern/src/test/java/raig/org/GumballStateMachineTest.java
@@ -1,6 +1,20 @@
 package raig.org;
 
+import junit.framework.Assert;
+import org.junit.Test;
+
 public class GumballStateMachineTest {
 
+  @Test
+  public void first_State_Should_be_Initial(){
+
+    InController inController = new InController();
+    OutController outController = new OutController();
+
+    GumballStateMachine gumballStateMachine = new GumballStateMachine(outController,inController);
+    Assert.assertEquals(gumballStateMachine.getState(),GumballStateMachine.INITIAL_STATE);
+
+
+  }
 
 }
